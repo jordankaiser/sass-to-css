cc21cef673a1c8d8b7791780eb66d957b59ecd2f
Jordan Kaiser <jkmacbook@Jordans-MacBook-Pro.local>
2023-09-30 14:08:20 -0500

    initial

--- index.js
+++ index.js
@@ -1,27 +1,38 @@
-
-//requiring path and fs modules
-const path = require('path');
 const fs = require('fs');
-//joining path of directory 
-const directoryPath = path.join(__dirname, 'files');
-//passsing directoryPath and callback function
-fs.readdir(directoryPath, function (err, files) {
-  //handling error
-  if (err) {
-    return console.log('Unable to scan directory: ' + err);
-  }
-  const needle = 'watermelon';
-  //listing all files using forEach
-  files.forEach(function (file) {
-    // Do whatever you want to do with the file
-    const filePath = path.join(directoryPath, file);
-    fs.readFile(filePath, 'utf8', function (err, data) {
-      if (err) throw err;
-      if (data.includes(needle)) {
-        console.log(data)
-      } else {
-        console.log(`${needle} not found in ${file}`)
-      }
+const path = require('path');
+
+const directoryPath = './your_directory_path'; // Replace with the path to your directory
+const searchText = 'old_text'; // Replace with the text you want to find
+const replaceText = 'new_text'; // Replace with the text you want to replace
+
+function scanAndReplace(directoryPath, searchText, replaceText) {
+  fs.readdir(directoryPath, (err, files) => {
+    if (err) {
+      console.error(`Error reading directory: ${err}`);
+      return;
+    }
+
+    files.forEach((file) => {
+      const filePath = path.join(directoryPath, file);
+
+      fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+          console.error(`Error reading file ${filePath}: ${err}`);
+          return;
+        }
+
+        const updatedData = data.replace(new RegExp(searchText, 'g'), replaceText);
+
+        fs.writeFile(filePath, updatedData, 'utf8', (err) => {
+          if (err) {
+            console.error(`Error writing file ${filePath}: ${err}`);
+            return;
+          }
+          console.log(`Updated file: ${filePath}`);
+        });
+      });
     });
   });
-});
+}
+
+scanAndReplace(directoryPath, searchText, replaceText);
