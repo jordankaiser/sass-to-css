0b34423141aad57f68851515a125ec3a1ad50de5
Jordan Kaiser <jkmacbook@Jordans-MacBook-Pro.local>
2023-10-01 13:35:55 -0500

    remove comments and test code

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const fs = require('fs');
-// const util = require('util');
 const path = require('path');
 const readline = require('readline');
 const escapeRegexp = require("escape-string-regexp-node");
-// const readdir = util.promisify(fs.readdir);
 
 function camelToKebab(camelCase) {
   return camelCase.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
@@ -46,15 +44,9 @@ const directoryPath = path.join(__dirname, 'files'); // Replace with the path to
 
 function initFindAndReplace(variableMap) {
   if (variableMap.length > 0) {
-    // console.log('variableMap as param', variableMap);
     Array.from(variableMap).forEach((variable) => {
-      // console.log('variable: ', variable);
       const key = Object.keys(variable)[0];
       const value = variable[key];
-      // console.log('key: ', key);
-      // console.log('value: ', value);
-      // console.log('directoryPath: ', directoryPath);
-      // console.log('---');
       scanAndReplace(directoryPath, key, value);
     });
   }
@@ -67,16 +59,8 @@ function scanAndReplace(directoryPath, searchText, replaceText) {
       const filePath = path.join(directoryPath, fileName);
       try {
         const file = fs.readFileSync(filePath, 'utf8');
-        console.log('file typeof', typeof file);
-        // console.log(`Replacing ${searchText} with ${replaceText} in ${filePath}`);
-        // console.log('typeof searchText', typeof searchText);
-        // console.log('typeof replaceText', typeof replaceText);
-  
         const escapedSearchText = escapeRegexp(searchText);
-        const escapedReplaceText = escapeRegexp(replaceText);
         const updatedData = file.replace(new RegExp(escapedSearchText, 'g'), replaceText);
-        console.log('updatedData: ', updatedData);
-        console.log('---');
   
         try {
           fs.writeFileSync(filePath, updatedData, 'utf8');
@@ -93,47 +77,4 @@ function scanAndReplace(directoryPath, searchText, replaceText) {
   } catch (error) {
     console.error('Error reading directory:', error);
   }
-  // fs.readdir(directoryPath, (err, files) => {
-  //   if (err) {
-  //     console.error(`Error reading directory: ${err}`);
-  //     return;
-  //   }
-
-  //   files.forEach((file) => {
-  //     const filePath = path.join(directoryPath, file);
-
-  //     fs.readFile(filePath, 'utf8', (err, data) => {
-  //       if (err) {
-  //         console.error(`Error reading file ${filePath}: ${err}`);
-  //         return;
-  //       }
-
-  //       // console.log(`Replacing ${searchText} with ${replaceText} in ${filePath}`);
-  //       // console.log('typeof searchText', typeof searchText);
-  //       // console.log('typeof replaceText', typeof replaceText);
-
-  //       const escapedSearchText = escapeRegexp(searchText);
-  //       const escapedReplaceText = escapeRegexp(replaceText);
-  //       const updatedData = data.replace(new RegExp(escapedSearchText, 'g'), replaceText);
-  //       console.log('updatedData: ', updatedData);
-  //       console.log('---');
-
-  //       try {
-  //         fs.writeFileSync(filePath, updatedData, 'utf8');
-  //         console.log('File written successfully.');
-  //       } catch (error) {
-  //         console.error('Error writing file:', error);
-  //       }
-  //     });
-  //   });
-  // });
 }
-
-const originalText = "This is a $foo text with $foo data.";
-
-// Replace all occurrences of "sample" with "replacement"
-const searchTextBlah = "$foo";
-const replacementText = "var(--foo)";
-const modifiedText = originalText.replace(new RegExp(searchTextBlah, 'g'), replacementText);
-
-// console.log(modifiedText); // Output: "This is a replacement text with replacement data."
