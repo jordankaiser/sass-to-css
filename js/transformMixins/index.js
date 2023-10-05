4b8183b0b48e3e0784f82ce4695ab5c0ffea4c83
Jordan Kaiser <jkmacbook@Jordans-MacBook-Pro.local>
2023-10-04 19:46:16 -0500

    comment

--- js/transformMixins/index.js
+++ js/transformMixins/index.js
@@ -6,9 +6,9 @@ async function initTransformMixin(file) {
   // TODO: We have to have all the css in one file so things like $variables work.
   // Could do in such a way so it would be easy to split out later?
   // const result = await sass.compileAsync(file);
-  console.log('\nresult');
-  console.log(result);
-  console.log('---');
+  // console.log('\nresult');
+  // console.log(result);
+  // console.log('---');
 }
 
 module.exports = initTransformMixin;
