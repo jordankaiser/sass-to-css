3e6ce74cc9a7adb78a08ddb814da1e6ae604e1e3
Jordan Kaiser <jordan.kaiser@thinkshout.com>
2023-10-10 10:09:52 -0500

    remove transform mixins

--- index.js
+++ index.js
@@ -1,19 +1,4 @@
 const path = require('path');
 const transformVariables = require('./js/transformVariables/index.js');
-const initTransformMixin = require('./js/transformMixins/index.js');
 
 transformVariables(path.join(__dirname, 'variables', 'index.scss'));
-
-async function goTransformers() {
-  const files = [
-    path.join(__dirname, 'variables', 'index.scss'),
-    path.join(__dirname, 'files', 'file-one.scss'),
-  ]
-  console.log('__dirname', __dirname);
-  for (const file of files) {
-    console.log('file', file);
-    await initTransformMixin(file);
-  }
-}
-
-goTransformers();
