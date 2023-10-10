eff26d7230d7cdd4b6f61956d23b86bd1a5be5a7
Jordan Kaiser <jordan.kaiser@thinkshout.com>
2023-10-10 13:01:54 -0500

    comments and cleanup

--- js/createCustomProps/index.js
+++ js/createCustomProps/index.js
@@ -1,4 +1,3 @@
-const fs = require('fs');
 const {appendText} = require('./../_helpers/index.js');
 
 /**
@@ -8,10 +7,15 @@ const {appendText} = require('./../_helpers/index.js');
  * @param {string} filePath - The output file path for the custom properties.
  */
 async function createCustomProps(variableMap, filePath) {
+  // Add the first line.
   await appendText(filePath, '\n:root {');
+
+  // Loop through each variable and add it to the file.
   for (const variable of variableMap) {
     await appendText(filePath, `\n  ${variable.customPropName}: ${variable.value};`);
   }
+
+  // Add the last line.
   await appendText(filePath, '\n}');
 }
 
