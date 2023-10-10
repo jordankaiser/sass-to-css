98cd8df37883555d633d5e0f7001cdade9365acd
Jordan Kaiser <jordan.kaiser@thinkshout.com>
2023-10-10 11:53:26 -0500

    Promisify transform variables entry point

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const path = require('path');
 const transformVariables = require('./js/transformVariables/index.js');
 
-const foo = transformVariables(path.join(__dirname, 'variables', 'index.scss'));
-const bar = foo;
+/**
+ * An array of objects containing the SCSS key, custom property key, and value.
+ */
+let data = null;
+
+/**
+ * Initialize.
+ */
+async function init() {
+  data = await transformVariables(path.join(__dirname, 'variables', 'index.scss'))
+  const foo = data;
+  const bar = 'hi';
+}
+init();
--- js/transformVariables/index.js
+++ js/transformVariables/index.js
@@ -13,55 +13,61 @@ function camelToKebab(camelCase) {
  * @param {string} filePath - The directory containing the scss.
  * @return {array} An array of objects containing the SCSS key, custom property key, and value.
  */
-function transformVariables(filePath) {
-  const variableMap = [];
+async function transformVariables(filePath) {
+  return new Promise((resolve, reject) => {
+    try {
+      const variableMap = [];
 
-  // Create a readable stream.
-  const rl = readline.createInterface({
-    input: fs.createReadStream(filePath),
-    output: process.stdout,
-    terminal: false,
-  });
-  
-  // Read each line.
-  rl.on('line', (line) => {
-    const data = {};
-
-    // Get the SCSS key. This regex matches the first word followed by a colon. 
-    const getKey = line.match(/^(.*?):/);
-    if (getKey) {
-
-      // SCSS Key.
-      const scssName = getKey[1];
-      if (scssName) {
-        data.scssKey = scssName;
-      }
-
-      // Custom Property Key. Convert the SCSS key to kebab case and remove the first character.
-      const customPropName = `${camelToKebab(getKey[1]).substring(1)}`;
-      if (customPropName) {
-        data.customPropKey = `var(--${customPropName})`;
-      }
+      // Create a readable stream.
+      const rl = readline.createInterface({
+        input: fs.createReadStream(filePath),
+        output: process.stdout,
+        terminal: false,
+      });
+      
+      // Read each line.
+      rl.on('line', (line) => {
+        const data = {};
+    
+        // Get the SCSS key. This regex matches the first word followed by a colon. 
+        const getKey = line.match(/^(.*?):/);
+        if (getKey) {
+    
+          // SCSS Key.
+          const scssName = getKey[1];
+          if (scssName) {
+            data.scssKey = scssName;
+          }
+    
+          // Custom Property Key. Convert the SCSS key to kebab case and remove the first character.
+          const customPropName = `${camelToKebab(getKey[1]).substring(1)}`;
+          if (customPropName) {
+            data.customPropKey = `var(--${customPropName})`;
+          }
+        }
+    
+        // Value. This regex matches the first word after a space and before a semicolon.
+        const value = line.match(/(?<=\s)[^;]+/g);
+        if (value) {
+          data.value = value[0];
+        }
+    
+        if (data.scssKey && data.customPropKey && data.value) {
+          variableMap.push(data);
+        }
+      });
+      
+      rl.on('close', () => {
+        if (variableMap.length === 0) {
+          throw new Error('No variables found.');
+        }
+        initFindAndReplace(variableMap);
+        createCustomProps(variableMap);
+        resolve(variableMap);
+      });
+    }  catch (error) {
+      reject(error);
     }
-
-    // Value. This regex matches the first word after a space and before a semicolon.
-    const value = line.match(/(?<=\s)[^;]+/g);
-    if (value) {
-      data.value = value[0];
-    }
-
-    if (data.scssKey && data.customPropKey && data.value) {
-      variableMap.push(data);
-    }
-  });
-  
-  rl.on('close', () => {
-    if (variableMap.length === 0) {
-      throw new Error('No variables found.');
-    }
-    initFindAndReplace(variableMap);
-    createCustomProps(variableMap);
-    return variableMap;
   });
 }
 
