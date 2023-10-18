e23407fd2981284c4904060e10dfcdd7171dc143
Jordan Kaiser <jordan.kaiser@thinkshout.com>
2023-10-18 11:40:55 -0500

    add config file

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ const createVariableMap = require('./js/transformVariables/index.js');
 const createCustomProps = require('./js/createCustomProps/index.js');
 const findAndReplace = require('./js/findAndReplace/index.js');
 const question = require('./js/prompts/index.js');
+const {
+  customPropertiesPath,
+  customPropertiesFileName,
+  scssVariablesFile,
+  scssFiles,
+} = require('./scss-to-css.config.js');
+
+console.log(customPropertiesPath);
+console.log(customPropertiesFileName);
+console.log(scssVariablesFile);
+console.log(scssFiles);
 
 /**
  * An array of objects containing the SCSS key, custom property key, and value.
@@ -14,19 +25,19 @@ let variableMap = null;
  * The output file path for the custom properties. Customize as needed.
  */
 const outputFileInfo = {
-  path: path.join(__dirname, 'custom-props'),
-  file: 'custom-props.css',
+  path: path.join(__dirname, customPropertiesPath),
+  file: customPropertiesFileName,
 }
 
 /**
  * The SCSS variables file path. Customize as needed.
  */
-const sassVariablesFile = path.join(__dirname, 'variables', 'index.scss');
+const sassVariablesFile = path.join(__dirname, scssVariablesFile);
 
 /**
  * The directory containing the SCSS files to transform. Customize as needed.
  */
-const sassFiles = path.join(__dirname, 'files');
+const sassFiles = path.join(__dirname, scssFiles);
 
 /**
  * Initialize.
---
+++ scss-to-css.config.js
@@ -0,0 +1,26 @@
+/**
+ * The folder to output the custom properties file to. Relative to this directory.
+ */
+const customPropertiesPath = 'custom-props';
+
+/**
+ * The desired file name of the custom properties file.
+ */
+const customPropertiesFileName = 'custom-props.css';
+
+/**
+ * The location of the SCSS variables file. Relative to this directory.
+ */
+const scssVariablesFile = 'variables/index.scss';
+
+/**
+ * The location of the SCSS files to transform. Relative to this directory.
+ */
+const scssFiles = 'files';
+
+module.exports = {
+  customPropertiesPath,
+  customPropertiesFileName,
+  scssVariablesFile,
+  scssFiles,
+};
