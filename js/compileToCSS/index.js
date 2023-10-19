bc1406fad6c74833cc1efc9fbc85ed8306f090fb
Jordan Kaiser <jordan.kaiser@thinkshout.com>
2023-10-19 07:17:05 -0500

    revert dart-sass attempt

--- js/compileToCSS/index.js
+++ js/compileToCSS/index.js
@@ -1,31 +1,6 @@
 const fs = require('fs');
 const {glob} = require('glob');
-const {getFileText} = require('../_helpers/index.js');
-const sass = require('sass');
-const path = require('path');
-
-async function compileToCSS(scssFilePath, ignoreDirectoryPaths) {
-  await processFiles(scssFilePath, ignoreDirectoryPaths);
-  // await compile(file);
-}
-
-async function compile(file) {
-  // const outputPath = inputPath.replace('.scss', '.css');
-  const inputPath = path.join(__dirname, '..', '..', 'files', 'file-one.scss');
-  const outputPath = path.join(__dirname, '..', '..', 'files', 'file-one.css');
-
-  console.log('inputPath', typeof inputPath);
-  console.log('outputPath', typeof outputPath);
-
-  await sass.compileAsync(inputPath)
-    .then((result) => {
-      fs.writeFileSync(outputPath, result.css, 'utf8');
-      console.log(`Sass file compiled successfully: ${inputPath} -> ${outputPath}`);
-    })
-    .catch((error) => {
-      console.error('Sass compilation error:', error);
-    });
-}
+const {getFileText} = require('./../_helpers/index.js');
 
 /**
  * Add comments to files to indicate where they where created.
@@ -33,7 +8,7 @@ async function compile(file) {
  * @param {string} scssFilePath - The path to the SCSS files.
  * @param {array} ignoreDirectories - The directories to ignore.
  */
-async function processFiles(scssFilePath, ignoreDirectoryPaths) {
+async function addFileComments(scssFilePath, ignoreDirectoryPaths) {
   const directories = await glob(scssFilePath + '/**/*.scss', { ignore: ignoreDirectoryPaths });
   const filesText = [];
   for (const file of directories) {
@@ -45,23 +20,22 @@ async function processFiles(scssFilePath, ignoreDirectoryPaths) {
   }
   for (const file of filesText) {
     if (file.text && file.path) {
-      const fileWithComments = insertImport(file);
+      const fileWithComments = insertComments(file);
       fs.writeFileSync(file.path, fileWithComments, 'utf8');
     }
   }
 }
 
-// /Users/jkmacbook/Sites/sass-to-css/files/file-one.scss
-
 /**
  * Adds a comment to the top and bottom of a file.
  *
  * @param {string} text - The text that needs comments.
- * @return {string} The text with import inserted.
+ * @return {string} The text with comments inserted.
  */
-function insertImport(text) {
-  const importText = `@import '../variables/index.scss';\n`;
-  return importText + text.text;
+function insertComments(text) {
+  const commentTop = `/* REPLACER_START ${text.path} */\n`;
+  const commentBottom = `\n/* REPLACER_END ${text.path} */`;
+  return commentTop + text.text + commentBottom;
 }
 
-module.exports = compileToCSS;
+module.exports = addFileComments;
