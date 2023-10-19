fba2b70fd7905a66800b64f2d08f19b08962d45e
Jordan Kaiser <jordan.kaiser@thinkshout.com>
2023-10-19 07:56:14 -0500

    snap

--- scss-to-css.config.js
+++ scss-to-css.config.js
@@ -1,26 +1,26 @@
 // The folder to output the custom properties file to. Relative to this directory.
-const customPropertiesPath = 'variables';
-// const customPropertiesPath = '../sass/00_global';
+// const customPropertiesPath = 'variables';
+const customPropertiesPath = '../sass/00_global';
 
 // The desired file name of the custom properties file.
 const customPropertiesFileName = 'custom-props.css';
 
 // The location of the SCSS variables file. Relative to this directory.
-const scssVariablesFile = 'variables/index.scss';
-// const scssVariablesFile = '../sass/00_global/_01.generated-vars.scss';
+// const scssVariablesFile = 'variables/index.scss';
+const scssVariablesFile = '../sass/00_global/_01.generated-vars.scss';
 
 // The location of the SCSS files to transform. Relative to this directory.
-const scssFiles = 'files';
-// const scssFiles = '../sass/';
+// const scssFiles = 'files';
+const scssFiles = '../sass/';
 
 // Directores to be ignored. If no directories are to be ignored put an empty array.
 // TODO: Write comment out more once this is implemented.
-const ignoreDirectories = [];
-// const ignoreDirectories = ['../sass/lib/**', '../sass/print/**', '../sass/select2-theme/**', '../sass/style.scss', '../sass/select2-theme.scss', '../sass/00_global/_01.generated-vars.scss'];
+// const ignoreDirectories = [];
+const ignoreDirectories = ['../sass/lib/**', '../sass/print/**', '../sass/select2-theme/**', '../sass/style.scss', '../sass/select2-theme.scss', '../sass/00_global/_01.generated-vars.scss'];
 
 // The compiled CSS file. Relative to this directory.
-const cssFile = 'dist/index.css';
-// const cssFile = '../css/style.css';
+// const cssFile = 'dist/index.css';
+const cssFile = '../css/style.css';
 
 module.exports = {
   customPropertiesPath,
